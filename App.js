fc1bf4b99da8609ef100860595a58979d649cb0a
shunkosa <shun.kosaka.102@gmail.com>
2020-03-06 15:57:17 +0900

    Load custom fonts

--- App.js
+++ App.js
@@ -1,63 +1,29 @@
 import React, { PureComponent } from 'react';
-import { AppState } from 'react-native';
-import { openDatabase, closeDatabase } from './src/services/Database';
+import * as Font from 'expo-font';
 import Router from './src/Router';
 import { initializeStorage } from './src/utility'; 
 
+console.disableYellowBox = true;
+initializeStorage();
 export default class App extends PureComponent {
   state = {
-    appState: AppState.currentState,
-    isDatabaseReady: false,
-  };
+    fontLoaded: false
+  }
 
-  componentDidMount = async () => {
-    console.disableYellowBox = true;
-    this.appIsNowRunningInForeground();
-    this.setState({
-      appState: 'active'
+  async componentDidMount() {
+    await Font.loadAsync({
+      "SalesforceSans-Regular": require('./assets/fonts/SalesforceSans-Regular.ttf'),
+      "SalesforceSans-Bold": require('./assets/fonts/SalesforceSans-Bold.ttf'),
+      "SalesforceSans-Light": require('./assets/fonts/SalesforceSans-Light.ttf')
     });
-    // Listen for app state changes
-    AppState.addEventListener('change', this.handleAppStateChange);
-    await initializeStorage();
-  };
 
-  componentWillUnmount = () => {
-    // Remove app state change listener
-    AppState.removeEventListener('change', this.handleAppStateChange);
-  };
-
-  handleAppStateChange = nextAppState => {
-    if (
-      this.state.appState.match(/inactive|background/) &&
-      nextAppState === 'active'
-    ) {
-      // App has moved from the background (or inactive) into the foreground
-      this.appIsNowRunningInForeground();
-    } else if (
-      this.state.appState === 'active' &&
-      nextAppState.match(/inactive|background/)
-    ) {
-      // App has moved from the foreground into the background (or become inactive)
-      this.appHasGoneToTheBackground();
-    }
-    this.setState({ appState: nextAppState });
-  };
-
-  appIsNowRunningInForeground = async () => {
-    console.log('App is now running in the foreground!');
-    await openDatabase();
     this.setState({
-      databaseIsReady: true
+      fontLoaded: true
     });
-  };
-
-  appHasGoneToTheBackground = () => {
-    console.log('App has gone to the background.');
-    closeDatabase();
-  };
+  }
 
   render() {
-    if (this.state.isDatabaseReady) {
+    if (this.state.fontLoaded) { 
       return <Router />;
     }
     return null;
