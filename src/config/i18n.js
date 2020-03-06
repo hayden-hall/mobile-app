183c61a4a113b97b5072bca3eb069cd83e6e3ccd
shunkosa <shun.kosaka.102@gmail.com>
2020-03-06 11:24:51 +0900

    Housekeeping localization

---
+++ src/config/i18n.js
@@ -0,0 +1,15 @@
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+
+import ne from './locale/ne_IN.json';
+import en from './locale/en_US.json';
+
+//expo-localization gives the result in a language-region format
+const deviceLocale = (Localization.locale.indexOf("-") !==  0) ? deviceLocale.split("-")[0] : Localization.locale;
+
+i18n.defaultLocale = deviceLocale;
+i18n.locale = deviceLocale;
+i18n.fallbacks = true;
+i18n.translations = { en, ne };
+
+export default i18n;
