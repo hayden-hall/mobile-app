59c4d15d78a9197473c948f04ceb12cf8287c92e
shunkosa <shun.kosaka.102@gmail.com>
2020-04-04 11:55:58 +0900

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,7 @@ module.exports = {
   globals: {
     __DEV__: true,
   },
+  preset: 'jest-expo',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx', 'node'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base)',
-  ],
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
 };
