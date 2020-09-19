c468a639ef12d68c22ce7473f9852916b73c8281
shunkosa <shun.kosaka.102@gmail.com>
2020-09-19 12:52:29 +0900

    Add type of survey layout object

--- src/types/survey.ts
+++ src/types/survey.ts
@@ -1,6 +1,16 @@
-import { string } from 'prop-types';
-
 export interface Survey {
   subtitle: string;
   showCaret: boolean;
+}
+
+export interface SurveyLayout {
+  sections?: Array<{
+    id: string;
+    label: string;
+    items?: Array<{
+      name: string;
+      label: string;
+      type: string;
+    }>;
+  }>;
 }
