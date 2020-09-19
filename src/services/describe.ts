fbc559cab1c83305f01f8e5e8cdf322de2674a1a
shunkosa <shun.kosaka.102@gmail.com>
2020-09-19 12:54:10 +0900

    Add getLayoutDetail

--- src/services/describe.ts
+++ src/services/describe.ts
@@ -1,10 +1,11 @@
 import { describeLayoutResult, describeLayout } from './api/salesforce/core';
-import { saveRecords, getAllRecords } from './database';
+import { saveRecords, getAllRecords, getRecords } from './database';
 
 import { RecordType, PageLayoutSection, PageLayoutItem } from '../types/sqlite';
-import { logger } from '../utility/logger';
+import { SurveyLayout } from '../types/survey';
 import { DescribeLayoutResult, DescribeLayout } from '../types/metadata';
 
+import { logger } from '../utility/logger';
 import { DB_TABLE } from '../constants';
 import { SURVEY_OBJECT } from 'react-native-dotenv';
 
@@ -66,7 +67,49 @@ export const storePageLayoutItems = async (pageLayoutId: string) => {
   return response;
 };
 
-export const getAllRecordTypes = async () => {
-  const recordTypes: Array<any> = await getAllRecords(DB_TABLE.RecordType);
+export const getAllRecordTypes = async (): Promise<Array<RecordType>> => {
+  const recordTypes: Array<RecordType> = await getAllRecords(DB_TABLE.RecordType);
   return recordTypes;
+};
+
+/**
+ * Construct page layout object from locally stored page layout sections and items
+ * @param layoutId
+ */
+export const getLayoutDetail = async (layoutId: string): Promise<SurveyLayout> => {
+  // sections in the layout
+  const sections: Array<PageLayoutSection> = await getRecords(
+    DB_TABLE.PageLayoutSection,
+    `where layoutId='${layoutId}'`
+  );
+  // items used in the sections
+  const sectionIds = sections.map(s => s.id);
+  const items: Array<PageLayoutItem> = await getRecords(
+    DB_TABLE.PageLayoutItem,
+    `where sectionId in (${sectionIds.map(id => `'${id}'`).join(',')})`
+  );
+  logger('FINE', 'getLayoutDetail', items);
+
+  // group items by section id
+  const sectionIdToItems = items.reduce(
+    (result, item) => ({
+      ...result,
+      [item.sectionId]: [...(result[item.sectionId] || []), item],
+    }),
+    {}
+  );
+
+  const layout: SurveyLayout = {
+    sections: sections.map(s => ({
+      id: s.id,
+      label: s.sectionLabel,
+      items: sectionIdToItems[s.id].map(item => ({
+        name: item.fieldName,
+        label: item.fieldLabel,
+        type: item.fieldType,
+      })),
+    })),
+  };
+
+  return layout;
 };
