d0e91277b09ffde7582af579d10fc82095be5b8b
shunkosa <shun.kosaka.102@gmail.com>
2020-09-22 11:31:37 +0900

    Move localization context

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import LocalizationContext from './context/localizationContext';
 
 // screens
 import Login from './screens/login';
@@ -16,7 +18,6 @@ import { SettingsButton, LogoutButton } from './components/headerButtons';
 
 // styles
 import { APP_FONTS, APP_THEME } from './constants';
-import i18n from './config/i18n';
 
 const headerStyle = {
   headerStyle: {
@@ -45,70 +46,59 @@ export type StackParamList = {
   Settings: undefined;
 };
 
-export const LocalizationContext = React.createContext(null);
 const Stack = createStackNavigator();
 
 export default function Router() {
-  const [locale, setLocale] = React.useState(i18n.locale);
-  const localizationContext = React.useMemo(
-    () => ({
-      t: (scope, options) => i18n.t(scope, { locale, ...options }),
-      locale,
-      setLocale,
-    }),
-    [locale]
-  );
+  const { t } = useContext(LocalizationContext);
 
   return (
-    <LocalizationContext.Provider value={localizationContext}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{
-              title: i18n.t('LOGIN'),
-              ...headerStyle,
-            }}
-          />
-          <Stack.Screen
-            name="AreaCode"
-            component={AreaCode}
-            options={{
-              title: i18n.t('ENTER_AREA_CODE'),
-              ...headerStyle,
-            }}
-          />
-          <Stack.Screen
-            name="SurveyList"
-            component={SurveyList}
-            options={({ navigation }) => ({
-              title: i18n.t('SURVEYS'),
-              headerLeft: () => LogoutButton(navigation),
-              headerRight: () => SettingsButton(navigation),
-              ...headerStyle,
-            })}
-          />
-          <Stack.Screen
-            name="SurveyTypePicker"
-            component={SurveyTypePicker}
-            options={{
-              title: i18n.t('CHOOSE_SURVEY'),
-              ...headerStyle,
-            }}
-          />
-          <Stack.Screen name="SurveyEditor" component={SurveyEditor} />
-          <Stack.Screen name="SurveyCompleted" component={SurveyCompleted} />
-          <Stack.Screen
-            name="Settings"
-            component={Settings}
-            options={{
-              title: i18n.t('SETTINGS'),
-              ...headerStyle,
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </LocalizationContext.Provider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: t('LOGIN'),
+            ...headerStyle,
+          }}
+        />
+        <Stack.Screen
+          name="AreaCode"
+          component={AreaCode}
+          options={{
+            title: t('ENTER_AREA_CODE'),
+            ...headerStyle,
+          }}
+        />
+        <Stack.Screen
+          name="SurveyList"
+          component={SurveyList}
+          options={({ navigation }) => ({
+            title: t('SURVEYS'),
+            headerLeft: () => LogoutButton(navigation),
+            headerRight: () => SettingsButton(navigation),
+            ...headerStyle,
+          })}
+        />
+        <Stack.Screen
+          name="SurveyTypePicker"
+          component={SurveyTypePicker}
+          options={{
+            title: t('CHOOSE_SURVEY'),
+            ...headerStyle,
+          }}
+        />
+        <Stack.Screen name="SurveyEditor" component={SurveyEditor} />
+        <Stack.Screen name="SurveyCompleted" component={SurveyCompleted} />
+        <Stack.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            title: t('SETTINGS'),
+            ...headerStyle,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
